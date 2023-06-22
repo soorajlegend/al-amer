f1cde7e0a5094b393e0c1358592b221cd8a2ec56
suraj muhammad <soorajwizard01@gmail.com>
2023-06-22 20:00:17 +0300

    added arabic to videoCard component

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -18,9 +18,9 @@ const MovieList: React.FC<MovieList> = ({ data, title }) => {
 
 
     return (
-        <div className='lg:px-4 md:px-12 mt-4 space-y-8'>
+        <div className='lg:px-4 md:px-12 space-y-8 mt-10 md:mt-4'>
             <div className={`text-white ${choose("text-left","text-right")}`}>
-                <p className="text-md md:text-xl lg:text-2xl font-semibold mb-4">{title}</p>
+                <p className="text-md md:text-xl lg:text-2xl px-5 lg:px-0 font-semibold mb-4">{title}</p>
                 <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2">
                     {
                         data.map((movie) => (
