f53e01bce45ba34589a56fde003e585bada17e43
suraj muhammad <soorajwizard01@gmail.com>
2023-06-22 18:36:03 +0300

    added arabic to the mobile menu components

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,27 +1,31 @@
 import React from 'react'
+import { useData } from './DataProvider';
 
 interface MobileMenuProps {
     visible: boolean
 }
 
-const MobileMenu: React.FC<MobileMenuProps> = ({visible}) => {
+const MobileMenu: React.FC<MobileMenuProps> = ({ visible }) => {
 
-    if(!visible){
+    if (!visible) {
         return null;
     }
 
-  return (
-    <div className='bg-black/70 backdrop-blur-md w-56 absolute top-8  left-0 py-5 flex flex-col shadow-lg rounded-md '>
-        <div className="flex flex-col gap-4 text-slate-100">
-            <div className="px-3 text-center capitalize hover:underline">Home</div>
-            <div className="px-3 text-center capitalize hover:underline">Series</div>
-            <div className="px-3 text-center capitalize hover:underline">Films</div>
-            <div className="px-3 text-center capitalize hover:underline">New and popular</div>
-            <div className="px-3 text-center capitalize hover:underline">my list</div>
-            <div className="px-3 text-center capitalize hover:underline">Browse by languages</div>
+    const { content, choose } = useData();
+
+
+    return (
+        <div className={`bg-black/70 backdrop-blur-md w-56 absolute top-8 ${choose("left-0","right-0")} py-5 flex flex-col shadow-lg rounded-md`}>
+            <div className="flex flex-col gap-4 text-slate-100">
+                <div className="px-3 text-center capitalize hover:underline">{content?.nav?.home!}</div>
+                <div className="px-3 text-center capitalize hover:underline">{content?.nav?.series!}</div>
+                <div className="px-3 text-center capitalize hover:underline">{content?.nav?.films!}</div>
+                <div className="px-3 text-center capitalize hover:underline">{content?.nav?.newAndPopular!}</div>
+                <div className="px-3 text-center capitalize hover:underline">{content?.nav?.myList!}</div>
+                <div className="px-3 text-center capitalize hover:underline">{content?.nav?.browseByLanguages!}</div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default MobileMenu
